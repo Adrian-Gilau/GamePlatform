8a758e88b84cddc36c42fca90cd69ad2d2ab5336
Your NameAdrianGilau <adriamgilau2.0@gmail.com>
2026-03-17 22:20:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+public class GamePlatform
+{
+    public static double CalculateFinalSpeed(double initalSpeed, int[] inclinations)
+    {
+
+        double affectedSpeed = initalSpeed;
+        foreach (var inclination in inclinations)
+        {
+            if (inclination == 30)
+            {
+                affectedSpeed -= 30;
+            } else if (inclination == -45)
+            {
+                affectedSpeed += 45;
+            }
+        }
+        return affectedSpeed;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CalculateFinalSpeed(60, new int[] { 0, 30, 0, -45, 0}));
+    }
+}
